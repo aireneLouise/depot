b0b706717f77a34f5549c099d863785b20ca5aaa
AireneLouise <airenelouise@airenelouise-emachines-D725.(none)>
2012-11-21 12:14:22 +0800

    Tab and Mass-assign Errors Fixed

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
@@ -11,16 +19,18 @@ class OrdersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:orders)
   end
 
-	test "requires item in cart" do
-		get :new
-		assert_redirected_to store_path
-		assert_equal flash[:notice], 'Your cart is empty'
-	end
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
 
   test "should get new" do
-		cart = Cart.create
-		session[:cart_id] = cart.id
-		LineItem.create(:cart => cart, :product => products(:ruby))
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
 
     get :new
     assert_response :success
@@ -28,11 +38,11 @@ class OrdersControllerTest < ActionController::TestCase
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
+      post :create, order: { address: @order.address, email: @order.email,
+        name: @order.name, pay_type: @order.pay_type }
     end
 
     assert_redirected_to store_path
-
   end
 
   test "should show order" do
