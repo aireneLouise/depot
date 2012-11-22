2de9fe702392bfce1f3c4115be84dc7b24ec0393
AireneLouise <airenelouise@airenelouise-emachines-D725.(none)>
2012-11-22 14:34:26 +0800

    Iteration J4: Add a Locale Switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,21 +23,21 @@ class ApplicationController < ActionController::Base
       end
     end
 
-protected
-def set_i18n_locale_from_params
-if params[:locale]
-if I18n.available_locales.include?(params[:locale].to_sym)
-I18n.locale = params[:locale]
-else
-flash.now[:notice] =
-"#{params[:locale]} translation not available"
-logger.error flash.now[:notice]
-end
-end
-end
-def default_url_options
-{ :locale => I18n.locale }
-end
+  protected
+    def set_i18n_locale_from_params
+       if params[:locale]
+         if I18n.available_locales.include?(params[:locale].to_sym)
+         I18n.locale = params[:locale]
+        else
+          flash.now[:notice] ="#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end 
+       end
+     end
+    
+     def default_url_options
+        { :locale => I18n.locale }
+     end
 
 
 end
